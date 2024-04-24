04004759aedbb9f0e9dd88cf57448d3a613969c2
SebSanUwU <juancamargo206@gmail.com>
2024-04-24 13:55:31 -0500

    Cambios para deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     backLink: 'https://cenizasback.azurewebsites.net',
     socketLink: 'https://cenizassockets.azurewebsites.net',
-    homeLink: 'https://cenizasdelpasado.azurewebsites.net',
+    homeLink: 'https://cenizasdelpasadogame.azurewebsites.net',
     production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment ={
     backLink: 'http://localhost:8080',
     socketLink: "http://localhost:2525",
-    homeLink: "http://localhost:4200",
+    homeLink: "http://localhost",
     production: false
 }
